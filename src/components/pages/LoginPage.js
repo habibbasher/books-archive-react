bb2fa2eeab1bfcbb2053e60bf17323a24617d0b5
habibbasher <habib0904010@gmail.com>
2017-12-18 13:44:52 +0600

    React routing and form validation is implemented

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+import LoginForm from '../forms/LoginForm';
+
+class LoginPage extends Component {
+
+  submit = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+
+        <LoginForm submit={this.submit} />
+      </div>
+
+    );
+  }
+}
+
+export default LoginPage;
